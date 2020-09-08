d8b045c56e8e931b3625645ed9d18f98e9d21194
KillerBe <killerbe@jmail.com>
2020-09-08 21:41:16 +0300

    add Xcode Project

---
+++ gitSample/gitSample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  gitSample
+//
+//  Created by Dima Khymych on 08.09.2020.
+//  Copyright © 2020 Dima Khymych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .cyan
+    }
+ 
+
+}
+
